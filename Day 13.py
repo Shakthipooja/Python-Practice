50d20d15c3168e09037c1c0129df6da5b67a368d
Shakthipooja <shakthipoojah@gmail.com>
2026-06-30 15:54:09 +0530

    Add files via upload

---
+++ Day 13.py
@@ -0,0 +1,37 @@
+#math module
+#Square root
+import math
+number = int(input("Enter a number: "))
+result = math.sqrt(number)
+print("Square root = ",result)
+#Factorial
+import math
+number = int(input("Enter a number: "))
+result = math.factorial(number)
+print("Factorial = ",result)
+#Power
+import math
+number = int(input("Enter a number: "))
+result = math.pow(base,power)
+print("Result = ",result)
+#Value of pi
+import math
+print("Value of pi =",math.pi)
+#Random number
+import math
+number = random.randint(1,100)
+print("Random number = ",number)
+#Random otp generation
+import random
+otp = random.randint(100000,999999)
+print("OTP = ",otp)
+#Dice simulator
+import random
+dice = random.randint(1,6)
+print("Dice rolled = ",dice)
+#Datetime Module
+#Print current date and time
+import datetime
+today = datetime.datetime.now()
+print("Current Date and Time: ")
+print(today)
